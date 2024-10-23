b525eb6ab19fd7e51a607431ab31aae37af3388d
Gabriel Chaldu <gabrielchaldu@gmail.com>
2024-10-23 14:56:24 -0300

    feat: servicio post finalizado

--- src/app/tareas/components/add-tarea/add-tarea.component.ts
+++ src/app/tareas/components/add-tarea/add-tarea.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { Tarea } from '../../interfaces/tarea.interface';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TareaService } from '../../service/tarea.service';
 
 @Component({
   selector: 'app-add-tarea',
@@ -17,6 +18,7 @@ export class AddTareaComponent {
 
 
   fb = inject(FormBuilder)
+  tareaService = inject(TareaService)
 
   formulario = this.fb.nonNullable.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],
@@ -24,12 +26,25 @@ export class AddTareaComponent {
   })
 
   emitiendoTarea() {
-    if (this.formulario.invalid) return
+    if (this.formulario.invalid) return;
     const tarea: Tarea = this.formulario.getRawValue();
-
+    this.guardarTarea(tarea);
     // Emitir una copia del objeto tarea para evitar el problema de referencia.
     this.tareaEmit.emit({ ...tarea });
     this.formulario.reset({ id: 0, nombre: '' });
+  }
+
+  guardarTarea(tarea: Tarea) {
+    this.tareaService.postTareas(tarea).subscribe(
+      {
+        next: (tarea: Tarea) => {
+          alert(`Tarea agregada con existo!!!`)
+        },
+        error: (e: Error) => {
+          console.log(e.message)
+        }
+      }
+    )
   }
 
 }
